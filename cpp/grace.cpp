a3216a77baac77b9413b81ba131941997ea45385
wassim <wassim@MBP-de-Admin.lan>
2023-04-03 00:54:28 +0200

    need to do asm

--- cpp/grace.cpp
+++ cpp/grace.cpp
@@ -1,36 +1,12 @@
-// #include <iostream>
-// #include <fstream>
-// #include <string>
-
-// #define FILENAME   "Grace_kid.cpp"
-// #define LAUNCHER() int main() { std::ifstream input(__FILE__); std::ofstream output_file(FILENAME); if (output_file){}; std::string line; while (std::getline(input, line)) { output_file << line << std::endl; } input.close();  output_file.close(); return 0; }
-
-// /*
-// This program will print its own source when run.
-// */
-
-// LAUNCHER()
-
 #include <iostream>
 #include <fstream>
 #include <string>
 
 #define FILENAME   "Grace_kid.cpp"
-//#define LAUNCHER() 
+#define LAUNCHER() int main() { std::ifstream input(__FILE__); std::ofstream output_file(FILENAME); if (output_file){}; std::string line; while (std::getline(input, line)) { output_file << line << std::endl; } input.close();  output_file.close(); return 0; }
 
 /*
 This program will print its own source when run.
 */
 
-int main() {
-    std::ifstream input(__FILE__);
-    std::ofstream output_file(FILENAME);
-    if (output_file){}; 
-    std::string line;
-    while (std::getline(input, line)) {
-        output_file << line << std::endl;
-    }
-    input.close();  
-    output_file.close();  
-    return 0;
-}
+LAUNCHER()
