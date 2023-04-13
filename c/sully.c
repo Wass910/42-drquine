975a1fdf22e55676fc989bebc938efc75c629609
wassim <wassim@MacBook-Pro-de-Admin.local>
2023-04-13 22:54:01 +0200

    just asm to do

--- c/sully.c
+++ c/sully.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-#define STRING "#include <stdio.h>%c%c#define STRING %c%s%c%c#define FILENAME %cSully_X.c%c%c%cint main() {%c    char filename[9] = FILENAME;%c    int i = %d;%c    FILE *file;%c%c    while (i > 0)%c    {%c        i--;%c        filename[6] = i + '0';%c        file = fopen(filename, %cw%c);%c        fprintf(file, STRING, 10, 10, 34, STRING, 34, 10, 34, 34, 10, 10,10, 10, i,  10, 10, 10, 10, 10, 10, 10, 34, 34, 10, 10, 10, 10);%c    }%c    return 0;%c}"
+#define STRING "#include <stdio.h>%c%c#define STRING %c%s%c%c#define FILENAME %cSully_X.c%c%c%cint main() {%c    char filename[9] = FILENAME;%c    int i = %d;%c    FILE *file;%c%c    if (i > 0){%c        while (i > 0)%c        {%c            i--;%c            filename[6] = i + '0';%c            file = fopen(filename, %cw%c);%c            fprintf(file, STRING, 10, 10, 34, STRING, 34, 10, 34, 34, 10, 10,10, 10, i,  10, 10, 10, 10, 10, 10, 10, 10, 34, 34, 10, 10, 10, 10, 10);%c        }%c    }%c    return 0;%c}"
 #define FILENAME "Sully_X.c"
 
 int main() {
@@ -8,12 +8,14 @@ int main() {
     int i = 5;
     FILE *file;
 
-    while (i > 0)
-    {
-        i--;
-        filename[6] = i + '0';
-        file = fopen(filename, "w");
-        fprintf(file, STRING, 10, 10, 34, STRING, 34, 10, 34, 34, 10, 10,10, 10, i,  10, 10, 10, 10, 10, 10, 10, 34, 34, 10, 10, 10, 10);
+    if (i > 0){
+        while (i > 0)
+        {
+            i--;
+            filename[6] = i + '0';
+            file = fopen(filename, "w");
+            fprintf(file, STRING, 10, 10, 34, STRING, 34, 10, 34, 34, 10, 10,10, 10, i,  10, 10, 10, 10, 10, 10, 10, 10, 34, 34, 10, 10, 10, 10, 10);
+        }
     }
     return 0;
 }
