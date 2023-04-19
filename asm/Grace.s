e3d95974d7d537dd0c4a17fc041d2245ea777c67
wass <wassimdhibaa@gmail.com>
2023-04-19 02:45:32 +0000

    done with the bonus

--- asm/Grace.s
+++ asm/Grace.s
@@ -1,6 +1,6 @@
 %macro DATA 0
     section .data
-        msg         db '%4$cmacro DATA 0%1$c    section .data%1$c        msg         db %2$c%3$s%2$c, 0%1$c        filename    db "Grace_kid.s", 0%1$c        flag        db "w", 0%1$c%4$cendmacro%1$c%1$c%4$cmacro TEXT 0%1$c    section .text%1$c        global main%1$c        extern fopen, fprintf, exit%1$c%4$cendmacro%1$c%1$c%4$cmacro MAIN 0%1$c    main:%1$c        push rbp%1$c        mov rbp, rsp%1$c        mov rdi, filename%1$c        mov rsi, flag%1$c        call     fopen%1$c        mov rdi, rax%1$c        mov rsi, msg%1$c        mov rdx, 10%1$c        mov rcx, 39%1$c        mov r8,  msg%1$c        mov r9,  37%1$c        call     fprintf%1$c        pop      rbp%1$c        mov rdi, 0%1$c        call     exit%1$c%4$cendmacro%1$c%1$cTEXT%1$cDATA%1$cMAIN%1$c%1$c;one comment', 0
+        msg         db '%4$cmacro DATA 0%1$c    section .data%1$c        msg         db %2$c%3$s%2$c, 0%1$c        filename    db "Grace_kid.s", 0%1$c        flag        db "w", 0%1$c%4$cendmacro%1$c%1$c%4$cmacro TEXT 0%1$c    section .text%1$c        global main%1$c        extern fopen, fprintf, exit%1$c%4$cendmacro%1$c%1$c%4$cmacro MAIN 0%1$c    main:%1$c        push rbp%1$c        mov rbp, rsp%1$c        mov rdi, filename%1$c        mov rsi, flag%1$c        call     fopen%1$c        mov rdi, rax%1$c        mov rsi, msg%1$c        mov rdx, 10%1$c        mov rcx, 39%1$c        mov r8,  msg%1$c        mov r9,  37%1$c        call     fprintf%1$c        pop      rbp%1$c        mov rdi, 0%1$c        call     exit%1$c%4$cendmacro%1$c%1$cDATA%1$cTEXT%1$cMAIN%1$c%1$c;one comment', 0
         filename    db "Grace_kid.s", 0
         flag        db "w", 0
 %endmacro
@@ -30,8 +30,8 @@
         call     exit
 %endmacro
 
-TEXT
 DATA
+TEXT
 MAIN
 
 ;one comment
